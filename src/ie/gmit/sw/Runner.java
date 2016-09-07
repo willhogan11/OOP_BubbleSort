fe90a4fcfd3eee73e0d97182575f26a35a4be5a2
Will Hogan <willhogan11@hotmail.com>
2016-09-07 18:27:36 +0100

    Mutator methods now working properly in Runner class

--- src/ie/gmit/sw/Runner.java
+++ src/ie/gmit/sw/Runner.java
@@ -3,30 +3,32 @@ package ie.gmit.sw;
 public class Runner {
 
 	public static void main(String[] args) {
-	/*	Array array = new Array();
+		Array array = new Array(); // Create a new Array class object 
+		Sortable sort = new SortableImpl(); // Using Polymorphism, create a new SortableImpl object from the Sortable interface
 		
-		Sortable sort = new SortableImpl();
-		array.setArr(sort.initialise());
-		sort.sort(array.getArr());
+		array.setArr(sort.initialise()); // Send the newly initialised sort object as a parameter to the setArray mutator method in the Array class 
+		sort.sort(array.getArr()); // Get the Values using the getArray mutator method
 		
-		System.out.println("The Sorted Array");
-		sort.display(array.getArr());*/
+		System.out.println("The Sorted Array"); // Display Text
+		sort.display(array.getArr()); // Using the sort object, display the results on screen
 		
+		
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////		
 		// Working on Removing Duplicates from the array (Testing this Stackoverflow Snippet first)
-		int[] input = new int[]{1, 1, 3, 7, 7, 8, 9, 9, 9, 10};
+		/*int[] input = new int[]{1, 1, 3, 7, 7, 8, 9, 9, 9, 10};
 		int current = input[0];
 		boolean found = false;
 
-		for (int i = 0; i < input.length; i++) {
-		    if (current == input[i] && !found) {
+		for (int j = 0; j < input.length; j++) {
+			if (current == input[j] && !found) {
 		        found = true;
-		    } else if (current != input[i]) {
-		        System.out.print(" " + current);
-		        current = input[i];
+		    } else if (current != input[j]) {
+		        System.out.print(current + "\n");
+		        current = input[j];
 		        found = false;
 		    }
 		}
-		System.out.print(" " + current);
-		
+		System.out.print(current);*/
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	}
 }
--- src/ie/gmit/sw/SortableImpl.java
+++ src/ie/gmit/sw/SortableImpl.java
@@ -3,15 +3,16 @@ package ie.gmit.sw;
 
 public class SortableImpl implements Sortable {
 	
+	// Populate Array with random numbers and return the array itself
 	public int[] initialise() {
 		int[] arr = new int[20];
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = (int)(Math.random()*20 + 1);
-			// System.out.println(arr[i]);
 		}
 		return arr;
 	}
 	
+	// Manual Bubble sort, Very poor as has a Space Time complexity of O(n2)
 	public void sort(int[] arr) {
 		int temp = 0;
 		for (int i = 0; i < arr.length; i++) {
@@ -25,6 +26,7 @@ public class SortableImpl implements Sortable {
 		}
 	}
 
+	// When called, Displays the sorted array on screen
 	public void display(int[] arr) {
 		for (int i = 0; i < arr.length; i++) {
 			System.out.println(arr[i]);
